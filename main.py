d54cc0827df9305f9d8e199f4875cc6059a41326
Melvin A <melvinalmonte@gmail.com>
2022-11-21 19:40:00 -0500

    lambdafied script

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
+import os
 from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
 
-GQL_ENDPOINT = 'YOUR_GQL_ENDPOINT_URL'
-GQL_API_KEY = 'YOUR_GQL_API_KEY'
+GQL_ENDPOINT = os.getenv('GQL_ENDPOINT') # PASS YOUR GRAPHQL ENDPOINT AS AN ENVIRONMENT VARIABLE
+GQL_API_KEY = os.getenv('GQL_API_KEY') # PASS YOUR GRAPHQL KEY AS AN ENVIRONMENT VARIABLE
 
+def lambda_handler(event, context):
 
-# Select your transport with a defined url endpoint
-transport = AIOHTTPTransport(url=GQL_ENDPOINT, headers= {'x-api-key': GQL_API_KEY})
+    # Select your transport with a defined url endpoint
+    transport = AIOHTTPTransport(url=GQL_ENDPOINT, headers= {'x-api-key': GQL_API_KEY})
 
-# Create a GraphQL client using the defined transport
-client = Client(transport=transport, fetch_schema_from_transport=True)
+    # Create a GraphQL client using the defined transport
+    client = Client(transport=transport, fetch_schema_from_transport=True)
 
-# Define your query, this example is to crete a new tracker instance.
-query = gql(
-    """
-    mutation CreateTracker($input: CreateMyfitrackrv2DBInput!){
-        createMyfitrackrv2DB(input: $input){
-            id
-            count
+    # Define your query, this example is to crete a new tracker instance.
+    query = gql(
+        """
+        mutation CreateTracker($input: CreateMyfitrackrv2DBInput!){
+            createMyfitrackrv2DB(input: $input){
+                id
+                count
+            }
+        }
+        """
+    )
+
+    variables = {
+        "input": {
+            "id": "4",
+            "count": 1
         }
     }
-    """
-)
 
-variables = {
-    "input": {
-        "id": "1",
-        "count": 1
+
+    response = client.execute(query,variable_values=variables)
+
+    return {
+        'statusCode': 200,
+        'body': response
     }
-}
-
-
-# Execute the query on the transport
-result = client.execute(query,variable_values=variables)
-print(result)
